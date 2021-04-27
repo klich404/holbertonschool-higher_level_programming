b17a44191671c9dfc0b764709747a1d647a92638
klich404 <klich4010@gmail.com>
2021-04-27 18:58:23 -0500

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for x in range(0, 10):
     for y in range(0, 10):
         if x == 8 and y == 9:
             print("{}{}".format(x, y))
-        if y > x:
+        elif y > x:
             print("{}{}, ".format(x, y), end="")
