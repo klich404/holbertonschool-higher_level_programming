493a8b8fcd24f92e504d31486d86718e3f42cf9f
klich404 <klich4010@gmail.com>
2021-04-28 14:23:39 -0500

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-args = sys.argv
-if len(args) == 1:
-    print("{} arguments.".format(len(args) - 1))
-elif len(args) == 2:
-    print("{} argument:".format(len(args) - 1))
-else:
-    print("{} arguments:".format(len(args) - 1))
-    for x in range(len(args)):
-        if x != 0:
-            print("{:d}: {}".format(x, args[x]))
+if __name__ = "__main_-":
+    import sys
+    args = sys.argv
+    if len(args) == 1:
+        print("{} arguments.".format(len(args) - 1))
+    elif len(args) == 2:
+        print("{} argument:".format(len(args) - 1))
+    else:
+        print("{} arguments:".format(len(args) - 1))
+        for x in range(len(args)):
+            if x != 0:
+                print("{:d}: {}".format(x, args[x]))
