55b7f58f971a55e002903183a8786f26ebac5cc1
klich404 <klich4010@gmail.com>
2021-04-28 14:22:08 -0500

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ import sys
 args = sys.argv
 if len(args) == 1:
     print("{} arguments.".format(len(args) - 1))
+elif len(args) == 2:
+    print("{} argument:".format(len(args) - 1))
 else:
     print("{} arguments:".format(len(args) - 1))
     for x in range(len(args)):
