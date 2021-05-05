796f1186e1cf52b4dd975f598e8de6eba68bc996
klich404 <klich4010@gmail.com>
2021-05-05 13:45:56 -0500

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ if __name__ == "__name__":
     def print_list_integer(my_list=[]):
         x = 0
         for x in my_list:
-            print("{:s}".format(my_list[x]))
+            print("{}".format(x))
