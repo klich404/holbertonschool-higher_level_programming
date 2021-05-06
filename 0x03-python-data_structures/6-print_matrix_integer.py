865f11393ee2562d39cc95094e8673dcf1fe0f5a
klich404 <klich4010@gmail.com>
2021-05-06 11:56:11 -0500

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    s = 0
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            print("{:d}".format(matrix[x][y]), end="")
+            if y is not len(matrix[x]) - 1:
+                print(" ", end="")
+        print()
