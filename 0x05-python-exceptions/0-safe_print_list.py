37ed756690be58a0f87ddcfc5e467119e597cd79
klich404 <klich4010@gmail.com>
2021-05-17 13:18:08 -0500

    task 0 and readme

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for y in range(0, x):
+        try:
+            print("{}".format(my_list[y]), end="")
+            i += 1
+        except:
+            pass
+    print()
+    return(i)
