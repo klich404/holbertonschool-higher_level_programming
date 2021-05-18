a95088f3da635dff187c692d248fc50032fdb934
klich404 <klich4010@gmail.com>
2021-05-18 16:39:43 -0500

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This class defines a squeare"""
+
+
+class Square:
+    """A Squeare with size and area"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        a = self.__size * self.__size
+        return(a)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for x in range(self.__size):
+                print("#", end="")
+            print()
