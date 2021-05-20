593c799f26ffb1b66dd20c1c13d12e365dc916a9
klich404 <klich4010@gmail.com>
2021-05-20 18:34:40 -0500

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This class defines a rectangle"""
+
+
+class Rectangle:
+    """A Rectangle with width, height, area and perimeter"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return(0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        s = ""
+        if self.__width == 0 or self.__height == 0:
+            return
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    s += ("#")
+                s += '\n'
+            return s
