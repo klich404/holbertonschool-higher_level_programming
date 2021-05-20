bc8f3bab6116a7dd2e404a3aad065833cbf4205f
klich404 <klich4010@gmail.com>
2021-05-20 18:47:57 -0500

    task 3 correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """A Rectangle with width, height, area and perimeter"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
