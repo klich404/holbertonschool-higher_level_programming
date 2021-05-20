c777c4848f87ded79f9ec756633a91590f360ce1
klich404 <klich4010@gmail.com>
2021-05-20 18:49:39 -0500

    task 3 correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         if self.__width != 0 or self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    s += ("#")
+                    s += ('#')
                 if i != self.__height - 1:
                     s += '\n'
             return s
