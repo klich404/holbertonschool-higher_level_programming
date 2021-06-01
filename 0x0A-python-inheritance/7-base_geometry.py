34e45243b3052d0b62e60fb08fe5e3f500d70e9c
klich404 <klich4010@gmail.com>
2021-06-01 18:28:32 -0500

    task 7 commented

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-    a class BaseGeometry (based on 6-base_geometry.py)
+    a class
+    BaseGeometry
+    (based on 6-base_geometry.py)
 """
 
 
 class BaseGeometry:
-    """a class BaseGeometry"""
+    """
+    a class BaseGeometry
+    """
 
     def area(self):
         raise Exception("area() is not implemented")
