cf7aeabccffae62a6b5dcaf5889174d2f37a0803
klich404 <klich4010@gmail.com>
2021-06-01 18:26:12 -0500

    task 3 commented

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,4 +7,7 @@ the specified class ; otherwise False
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Prototype: def is_kind_of_class(obj, a_class):
+    """
     return(isinstance(obj, a_class))
