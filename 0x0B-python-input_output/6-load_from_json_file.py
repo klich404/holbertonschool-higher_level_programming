519cc03a1b89cd0f98f1b05cc9cb24ee9aa77f9b
klich404 <klich4010@gmail.com>
2021-06-02 17:26:11 -0500

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that creates
+an Object from a
+“JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Prototype: def load_from_json_file(filename):
+    """
+    with open(filename, "r") as file:
+        return json.load(file)
