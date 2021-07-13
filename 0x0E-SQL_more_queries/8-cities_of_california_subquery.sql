e6348e95e2ba592dc8de17b7ed6f5378d483e574
klich404 <klich4010@gmail.com>
2021-07-13 16:29:30 -0500

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that
 -- can be found in the database hbtn_0d_usa
-SELECT * FROM states WHERE id_state (SELECT id, name FROM cities WHERE id_state = 1);
+SELECT * FROM states WHERE (SELECT * FROM cities WHERE id_state = 1);
