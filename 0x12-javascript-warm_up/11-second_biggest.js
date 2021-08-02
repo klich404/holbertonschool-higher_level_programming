4c3d614c9c1c20985b394cf90fe5f8487526cae9
klich404 <klich4010@gmail.com>
2021-08-02 17:45:41 -0500

    task 11 correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 /* script that searches the second biggest integer in the list of arguments. */
 
-function nextBiggest(arr) {
-  let max = -Infinity, result = -Infinity;
+function nextBiggest (arr) {
+  let max = -Infinity; let result = -Infinity;
 
   for (const value of arr) {
-    const nr = Number(value)
+    const nr = Number(value);
 
     if (nr > max) {
-      [result, max] = [max, nr] // save previous max
+      [result, max] = [max, nr]; // save previous max
     } else if (nr < max && nr > result) {
       result = nr; // new second biggest
     }
