704cae35fdc61ea6afb2dd0237b322b936d0ba7c
klich404 <klich4010@gmail.com>
2021-08-02 16:59:31 -0500

    task 10 corrections

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,8 +5,8 @@ function factorial (a) {
   if (!a) {
     return (1);
   } else {
-    return(a * factorial(a - 1));
+    return (a * factorial(a - 1));
   }
 }
 
-console.log(factorial(parseInt(process.argv[2])))
+console.log(factorial(parseInt(process.argv[2])));
