7f46f38af26d5664372a36d4a3f3a4e072b74a21
klich404 <klich4010@gmail.com>
2021-08-02 14:54:53 -0500

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* script that prints 3 lines */
+
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'JavaScript is amazing';
+
+console.log(var1 + '\n' + var2 + '\n' + var3);
