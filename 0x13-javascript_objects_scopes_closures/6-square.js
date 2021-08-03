8b59afeb3aba889a5881880931509126d3562574
klich404 <klich4010@gmail.com>
2021-08-03 12:19:48 -0500

    task 6 correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,7 @@
 /*  a class Square that defines a square
 and inherits from Square of 5-square.js */
 
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
+class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
     this.size = size;
