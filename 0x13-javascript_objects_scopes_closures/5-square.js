88640883ccef5fa21cdeafc65fad463c67d9b025
klich404 <klich4010@gmail.com>
2021-08-03 11:26:19 -0500

    task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* a class Rectangle that defines a rectangle */
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
