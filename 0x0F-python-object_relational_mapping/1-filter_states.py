e14d28f79ce048fbb23646594b1775295bf7c671
klich404 <klich4010@gmail.com>
2021-08-06 11:45:04 -0500

    task 0, 1 and 2 with requeriment correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,22 +7,24 @@ lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-"""open database connection"""
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=argv[1], password=argv[2], db=argv[3], charset="utf8")
 
-"""prepare object cursor with cursor()"""
-cursor = db.cursor()
+if __name__ == "__main__":
+    """open database connection"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=argv[1], password=argv[2], db=argv[3], charset="utf8")
 
-"""execute SQL query with execute()"""
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    """prepare object cursor with cursor()"""
+    cursor = db.cursor()
 
-"""Fetch all the rows with fetchall()"""
-rows = cursor.fetchall()
+    """execute SQL query with execute()"""
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-for row in rows:
-	print(row)
+    """Fetch all the rows with fetchall()"""
+    rows = cursor.fetchall()
 
-"""disconnect from server"""
-cursor.close()
-db.close()
+    for row in rows:
+        print(row)
+
+    """disconnect from server"""
+    cursor.close()
+    db.close()
