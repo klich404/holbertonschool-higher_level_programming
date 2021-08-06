bc13ba49d32957b0dacc34d9c70936e0d715ded0
klich404 <klich4010@gmail.com>
2021-08-06 10:45:48 -0500

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+"""open database connection"""
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=argv[1], password=argv[2], db=argv[3], charset="utf8")
+
+"""prepare object cursor with cursor()"""
+cursor = db.cursor()
+
+"""execute SQL query with execute()"""
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+"""Fetch all the rows with fetchall()"""
+rows = cursor.fetchall()
+
+for row in rows:
+	print(row)
+
+"""disconnect from server"""
+cursor.close()
+db.close()
