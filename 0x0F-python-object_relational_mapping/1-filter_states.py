bc2762120eab6ad23942ccf56acd7bdaa887d82b
klich404 <klich4010@gmail.com>
2021-08-08 12:46:11 -0500

    task 1 correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     """execute SQL query with execute()"""
-    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%'\
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%'\
         ORDER BY states.id ASC")
 
     """Fetch all the rows with fetchall()"""
