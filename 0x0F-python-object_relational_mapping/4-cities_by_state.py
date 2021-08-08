72bb9ec29bef36a863353ce662242134ceb6b02a
klich404 <klich4010@gmail.com>
2021-08-08 12:59:16 -0500

    task 4 pep8 correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,12 @@ from sys import argv
 
 if __name__ == "__main__":
     """open database connection"""
-    db = MySQLdb.connect(host="localhost", port=3306,
-                    user=argv[1], password=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         db=argv[3],
+                         charset="utf8")
 
     """prepare object cursor with cursor()"""
     cursor = db.cursor()
@@ -18,7 +22,7 @@ if __name__ == "__main__":
     """execute SQL query with execute()"""
     cursor.execute("SELECT cities.name FROM states INNER JOIN cities ON\
         cities.state_id = states.id WHERE\
-            state.name = %s ORDER BY cities.id", (argv[4],))
+            state.name = %s ORDER BY cities.id", (name, ))
 
     """Fetch all the rows with fetchall()"""
     rows = cursor.fetchall()
