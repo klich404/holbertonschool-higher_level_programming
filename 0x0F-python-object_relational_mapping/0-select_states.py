c2772ebe1caf266bd3c42087997b1c0fc7ede518
klich404 <klich4010@gmail.com>
2021-08-08 12:33:58 -0500

    task 0 correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     """open database connection"""
-    db = MySQLdb.connect(host="127.0.0.1",
+    db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          password=argv[2],
