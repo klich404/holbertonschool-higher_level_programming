03efd64589143831394bb52a1a702fbf77c7c1e5
klich404 <klich4010@gmail.com>
2021-08-08 16:45:57 -0500

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import session, sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@127.0.0.1/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    dates = session.query(State.id, State.name).first()
+    if dates is None:
+        print("Nothing")
+    else:
+        print("{:d}: {}".format(dates[0], dates[1]))
