eb98638eeaf343c7189e299b965fe0ea1a904139
klich404 <klich4010@gmail.com>
2021-08-08 12:36:16 -0500

    task 1 correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,12 @@ from sys import argv
 
 if __name__ == "__main__":
     """open database connection"""
-    db = MySQLdb.connect(host="localhost", port=3306,
-                    user=argv[1], password=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         db=argv[3],
+                         charset="utf8")
 
     """prepare object cursor with cursor()"""
     cursor = db.cursor()
