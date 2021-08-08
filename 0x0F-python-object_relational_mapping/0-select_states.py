3a2c6a84b619c60975dd68a756e846ec6514cee7
klich404 <klich4010@gmail.com>
2021-08-08 12:32:45 -0500

    task 0 with pep8 corrections

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,18 @@
 """
 lists all states from the database hbtn_0e_0_usa
 """
-
-
 import MySQLdb
 from sys import argv
 
+
 if __name__ == "__main__":
     """open database connection"""
-    db = MySQLdb.connect(host="localhost", port=3306,
-                    user=argv[1], password=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(host="127.0.0.1",
+                         port=3306,
+                         user=argv[1],
+                         password=argv[2],
+                         db=argv[3],
+                         charset="utf8")
 
     """prepare object cursor with cursor()"""
     cursor = db.cursor()
