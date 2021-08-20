043993f7e36a2c3cff0e64b63414d753ba381e0d
klich404 <klich4010@gmail.com>
2021-08-20 16:57:02 -0500

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    with requests.get('https://intranet.hbtn.io/status') as response:
-        print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
+    html = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
