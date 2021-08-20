a694dce2489f0e0ca3f8f777755878ed05a878d5
klich404 <klich4010@gmail.com>
2021-08-20 16:35:25 -0500

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8)
+"""
+from urllib import request
+from sys import argv
+import urllib
+
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print(e.__dict__.get("code"))
