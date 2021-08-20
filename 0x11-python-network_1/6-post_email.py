1da631305381c08299aec6bd1107238b22dd6864
klich404 <klich4010@gmail.com>
2021-08-20 17:23:30 -0500

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    data = {"email": argv[2]}
+    html = requests.post(url, data)
+    print(html.text)
