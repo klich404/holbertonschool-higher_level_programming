ecd2282e6440d99ad8658527431aab454a1d6cd1
klich404 <klich4010@gmail.com>
2021-08-20 19:16:33 -0500

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,13 +3,11 @@
 takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter
 """
-import json
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-
     d = {'q': ""}
     if len(argv) > 1:
         d['q'] = argv[1]
