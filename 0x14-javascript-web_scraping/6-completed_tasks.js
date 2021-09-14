340a8aab703d8e21b3c5955cdd7f2b102f78d710
klich404 <klich4010@gmail.com>
2021-09-14 18:42:13 -0500

    task 6 - code correction

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,17 +3,18 @@
 
 const request = require('request');
 const url = process.argv[2];
-const dictionary = {}
+const dictionary = {};
 request(url, function (error, response, body) {
   if (error) return console.log('code: ', error);
   const ids = (JSON.parse(body));
   for (const users of ids) {
-      if (users.complete === true) {
-        if (dictionary[users.userId]) {
-            dictionary[users.userId]
-        } else {
-            dictionary[users.userId] = 1
-        }
+    if (users.completed) {
+      if (dictionary[users.userId]) {
+        dictionary[users.userId]++;
+      } else {
+      dictionary[users.userId] = 1;
       }
+    }
   }
+  console.log(dictionary);
 });
