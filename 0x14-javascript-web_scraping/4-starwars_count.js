8b56919546f16c69950583d933d747bc91b29da9
klich404 <klich4010@gmail.com>
2021-09-14 16:14:45 -0500

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/* Write a script that display the status code of a GET request. */
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films';
+request(url, function (error, response, body) {
+    if (error) return console.log('code: ', error);
+    const results = JSON.parse(body).results[0];
+    const characters = results.characters;
+    for (character of characters) {
+        if (character.endsWith("18/") === true){
+            request(character, function (error, response, body) {
+                if (error) return console.log('code: ', error);
+                const films = JSON.parse(body).films;
+                console.log(films.length);
+            });
+        }
+    }
+});
