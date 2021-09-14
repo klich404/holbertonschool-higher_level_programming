58f85c2df114bd9507f5063529617a8ea8117bf9
klich404 <klich4010@gmail.com>
2021-09-14 16:15:20 -0500

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,16 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films';
 request(url, function (error, response, body) {
-    if (error) return console.log('code: ', error);
-    const results = JSON.parse(body).results[0];
-    const characters = results.characters;
-    for (character of characters) {
-        if (character.endsWith("18/") === true){
-            request(character, function (error, response, body) {
-                if (error) return console.log('code: ', error);
-                const films = JSON.parse(body).films;
-                console.log(films.length);
-            });
-        }
+  if (error) return console.log('code: ', error);
+  const results = JSON.parse(body).results[0];
+  const characters = results.characters;
+  for (const character of characters) {
+    if (character.endsWith('18/') === true) {
+      request(character, function (error, response, body) {
+        if (error) return console.log('code: ', error);
+        const films = JSON.parse(body).films;
+        console.log(films.length);
+      });
     }
+  }
 });
