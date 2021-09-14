2753bc6f575baf8c2d293970e15550c2a9168581
klich404 <klich4010@gmail.com>
2021-09-14 16:29:06 -0500

    task 4 - code corrections

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 /* Write a script that display the status code of a GET request. */
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films';
+const url = process.argv[2];
 let films = 0;
 request(url, function (error, response, body) {
   if (error) return console.log('code: ', error);
