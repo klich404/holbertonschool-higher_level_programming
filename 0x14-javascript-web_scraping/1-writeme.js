5921eb82b79707f5d6d31397053323cfe298af52
klich404 <klich4010@gmail.com>
2021-09-14 12:57:38 -0500

    task 1 code corrections

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,5 +4,9 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   console.log(data);
 });
